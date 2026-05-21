f477b82a2c579c34b7b681465c8892c6069b39e2
Thomas Petzold <tomlux@gmx.de>
2026-05-21 07:02:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shipping-toolkit-v1';
+const CACHE_NAME = 'shipping-toolkit-v14';
 const ASSETS = [
     'index.html',
     'manifest.json',
@@ -6,10 +6,23 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
         })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
